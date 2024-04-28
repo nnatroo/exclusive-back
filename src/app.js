313597823ba949b0d6e5cb87946ea4f5a1415d79
nnatroo <nugonatroshvili@gmail.com>
2024-04-28 18:40:12 +0400

    EXCB-3: Fix CORS Policy Error for Images

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 
-require('dotenv').config();
+require('dotenv')
+  .config();
 
 const middlewares = require('./middlewares');
 const api = require('./api');
@@ -11,9 +12,15 @@ const api = require('./api');
 const app = express();
 
 app.use(morgan('dev'));
-app.use(helmet());
+app.use(helmet({
+  crossOriginResourcePolicy: false,
+}));
 app.use(cors());
 app.use(express.json());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+
+app.use(express.static('public'));
+app.use('/images', express.static('./images'));
 
 app.get('/', (req, res) => {
   res.json({
