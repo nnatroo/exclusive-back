5519618b4c53cabc70b8b6653dcf24977f585ab5
nnatroo <nugonatroshvili@gmail.com>
2024-05-27 19:12:53 +0400

    EXCB-4: Revert

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const emojis = require('./emojis');
 const products = require('./products');
 const menus = require('./menus');
@@ -6,11 +7,15 @@ const menus = require('./menus');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({ message: 'API/V1' });
+  res.json({
+    message: 'API/V1',
+  });
 });
 
 router.use('/emojis', emojis);
+
 router.use('/products', products);
+
 router.use('/menus', menus);
 
 module.exports = router;
