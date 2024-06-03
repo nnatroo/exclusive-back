ad2d2706c72f316a56e9dda9b2da0620d005235a
nnatroo <nugonatroshvili@gmail.com>
2024-06-03 19:17:11 +0400

    EXCB-6-2: Revert

--- src/services/database.js
+++ src/services/database.js
@@ -2,7 +2,7 @@ const { MongoClient } = require('mongodb');
 
 require('dotenv').config();
 
-const uri = `mongodb+srv://${process.env.MONGODB_USEERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.e5hbong.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.e5hbong.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri);
 
