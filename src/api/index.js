9b8e34be2adc5c3af7884e12493ba855bd4452dc
Lasha Giorgi Qutateladze <lashagiorgi07@gmail.com>
2024-06-09 23:15:05 +0400

    EXCF-15

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const emojis = require('./emojis');
 const products = require('./products');
 const menus = require('./menus');
+const productsSwiper = require('./productsSwiper')
+// const timers = require('./timers')
 
 const router = express.Router();
 
@@ -17,5 +19,11 @@ router.use('/emojis', emojis);
 router.use('/products', products);
 
 router.use('/menus', menus);
+
+router.use('/productsSwiper', productsSwiper)
+
+// router.use('/timers', timers)
+
+
 
 module.exports = router;
